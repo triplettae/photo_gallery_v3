f8e85b04029ad1e5d351e2a21f9926f13a1faed0
triplettae <atrip81@gmail.com>
2017-05-15 21:36:32 -0400

    Fixed arrows in or out based on screen ratio

--- js/main.js
+++ js/main.js
@@ -1,21 +1,136 @@
 var thumbnails = 'photos/thumbnails/';
+var metadataURL = 'photos/photo_captions.txt';
+var metadata = {};
+var photoRatio = 1.6; //width/height of photos
+var viewportWidth;
+var viewportHeight;
+var resizeViewportTimeout;
+
+/***
+ * NOTE: The mockup showed the navigation arrows outside the photo; however, I needed
+ * a way to detect when the screen ratio is less than that of the photos so I can pull
+ * the navigation arrows inside the photo instead of letting them stay outside because
+ * when the screen is taller than it is wide (portrait) the photo's edges are rendered
+ * right up next to the edge of the window. I initially tried to override the CSS of
+ * the lightbox but that was disastrous mainly because the plugin uses JS to dynamically
+ * update the styles and manually changing these values upsets the whole lightbox system.
+ */
+
+function resizeViewport() {
+  
+  viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  
+  if ((viewportWidth / viewportHeight) >= photoRatio) {
+    $('.lb-nav a.lb-next').addClass('landscape');
+    $('.lb-nav a.lb-prev').addClass('landscape');
+  } else {
+    $('.lb-nav a.lb-next').removeClass('landscape');
+    $('.lb-nav a.lb-prev').removeClass('landscape');
+  }
+  
+}
+
+$(window).on('resize', function () {
+  
+  clearTimeout(resizeViewportTimeout);
+  resizeViewportTimeout = setTimeout(resizeViewport, 200);
+  
+});
+
+/***
+ * NOTE: All of the metadata for the photos is provided in a txt file and I decided to extract
+ * all the data using JS instead of copying and pasting it into the markup. This felt more
+ * reusable anyway and allowed me to practice my JS programming skills!
+ */
 
 $.ajax({
   
-  url : thumbnails,
+  url: metadataURL,
   success: function (data) {
     
-    $(data).find('a').attr('href', function (index, url) {
-      if( url.match(/\.jpg$/) ) {
-        console.log(url);
-        var photo = url.replace('\/thumbnails', '').replace('\\thumbnails', '');
-        var thumbnail = url;
-        var filename = url.match(/[^\\\/]+$/)[0];
-        var number = filename.replace('.jpg', '');
-        $('#gallery').append('<a data-lightbox="' + number + '" href="' + photo + '"><img src="' + thumbnail + '"></a>');
+    var metaDataArray = data.split('\n');
+    
+    if ((metaDataArray.length + 1) % 4 == 0) { // length + 1 because there is no LF at end of file
+      
+      for (var index = 0; index < metaDataArray.length; index += 4) {
+        
+        metadata[metaDataArray[index]] = { 
+
+          'alt': metaDataArray[index+1],
+          'caption': metaDataArray[index+2]
+
+        };
+        
       }
+      
+    }
+    
+  }
+  
+});
+
+/***
+ * NOTE: I used regex matching to ensure the href attributes can contain either / or \
+ * because PCs and Macs use different slashes for directories and $.ajax does not
+ * standardize the returned URLs. Elements are constructed using the values pulled from
+ * the caption file above.
+ */
+
+$.ajax({
+  
+  url: thumbnails,
+  success: function (data) {
+    
+    $(data).find('a').attr('href', function (index, thumbnail) {
+      
+      if( thumbnail.match(/\.jpg$/) ) {
+        
+        var photo = thumbnail.replace('\/thumbnails', '').replace('\\thumbnails', '');
+        var filename = photo.match(/[^\\\/]+$/)[0];
+      
+        $('#gallery').append(
+          '<a ' +
+          'data-lightbox="gallery" ' +
+          'data-title="' + (metadata[filename] ? metadata[filename].caption : '') + '" ' +
+          'href="' + photo + '">' +
+          '<img src="' + thumbnail + '" ' +
+          'alt="' + (metadata[filename] ? metadata[filename].alt : '') + '"></a>');
+        
+      }
+      
     });
     
   }
+  
 });
 
+/***
+ * NOTE: I needed a way to add .landscape to the next and prev arrows to pull them inside
+ * of the photo if the screen ratio was less than that of the photo because the photo is
+ * right up against the edge of the screen in this case. When the ratio is equal to or
+ * greater than that of the photo, the arrows can fit in the margin (using negative margins)
+ * on either side of the photo. My first naive approach was to simply call resizeViewport()
+ * right after calling lightbox. However, there was be some async code going on because
+ * the next and prev elements did not exist yet when my function was called. Looking through
+ * lightbox.js I found the build function creates the elements in the DOM so I hijacked that
+ * function and added my own resizeViewport() after it in the code below.
+ */
+var _lightboxBuild = lightbox.build;
+
+lightbox.build = function() {
+
+  _lightboxBuild.apply(this, arguments);
+  
+  resizeViewport();
+  
+}
+
+lightbox.option({
+  
+  'resizeDuration': 200,
+  'wrapAround': true,
+  'showImageNumberLabel': false,
+  'alwaysShowNavOnTouchDevices': true
+  
+});
